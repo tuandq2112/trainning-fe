d15eef240475a1f0d22694adee16d42cf6ea2584
Nguyen Ba Dong <Dong.NB223912@sis.hust.edu.vn>
2023-03-31 16:12:56 +0700

    dong4

---
+++ report/main.js
@@ -0,0 +1,20 @@
+function myfunc(){
+    var div= document.getElementbyId("e");
+    div.remove();
+ }
+ function myfunc1(){
+     var tr1= document.getElementbyId("r");
+     tr1.remove();
+  }
+  function myfunc2(){
+     var div= document.getElementbyId("s");
+     div.remove();
+  }
+  function myfunc3(){
+     var div= document.getElementbyId("t");
+     div.remove();
+  }
+  function myfunc4(){
+     var div= document.getElementbyId("y");
+     div.remove();
+  }
