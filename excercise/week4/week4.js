643e366466964bff7fabaa671792638a9a88a308
Anh <hoangvietanh180503@gmail.com>
2023-04-01 01:01:00 +0700

    week4

--- excercise/week4/week4.js
+++ excercise/week4/week4.js
@@ -2,4 +2,28 @@ function del(row) {
     row.closest('tr').remove();
   }
   
-  
+function edit(button) {
+    var row = button.parentNode.parentNode;
+    var cells = row.getElementsByTagName("th");
+
+    for (var i = 0; i < cells.length - 1; i++) {
+        var value = cells[i].innerHTML;
+        cells[i].innerHTML = "<input type='text' value='" + value + "'>";
+    }
+
+    button.innerHTML = "Save";
+    button.setAttribute("onclick", "save(this)");
+}
+
+function save(button) {
+    var row = button.parentNode.parentNode;
+    var cells = row.getElementsByTagName("th");
+
+    for (var i = 0; i < cells.length - 1; i++) {
+        var value = cells[i].getElementsByTagName("input")[0].value;
+        cells[i].innerHTML = value;
+    }
+
+    button.innerHTML = "Edit";
+    button.setAttribute("onclick", "edit(this)");
+}
